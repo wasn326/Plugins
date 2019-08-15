6b1e07714346809edcf355e473f1ad1277987447
wagen <wagen@DESKTOP-KIDBLDN.Speedport_W_724V_09011603_06_003>
2019-08-15 11:55:16 +0200

    test

--- ItemInv/src/de/k1ngmagmar/iteminv/command/HelpCommand.java
+++ ItemInv/src/de/k1ngmagmar/iteminv/command/HelpCommand.java
@@ -19,11 +19,19 @@ public class HelpCommand implements CommandExecutor{
 			String help2 = cfg.getString("Help.2");
 			String help3 = cfg.getString("Help.3");
 			String help4 = cfg.getString("Help.4");
+			String help5 = cfg.getString("Help.5");
+			String help6 = cfg.getString("Help.6");
+			String help7 = cfg.getString("Help.7");
+			String help8 = cfg.getString("Help.8");
 			
 			p.sendMessage(help1);
 			p.sendMessage(help2);
 			p.sendMessage(help3);
 			p.sendMessage(help4);
+			p.sendMessage(help5);
+			p.sendMessage(help6);
+			p.sendMessage(help7);
+			p.sendMessage(help8);
 			
 		}
 		return false;
